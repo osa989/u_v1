8e36cc312a72a8e2d7b0b1510039cb0ef65863a5
osa989 <osamagamal9988@gmail.com>
2024-02-17 08:11:29 +0200

    finish custom switcher menu design

--- lib/features/chat/presentation/view/widgets/chat_custom_app_bar.dart
+++ lib/features/chat/presentation/view/widgets/chat_custom_app_bar.dart
@@ -14,43 +14,76 @@ class ChatCustomAppBar extends StatelessWidget {
             bottomRight:
                 Radius.circular(20)), // Set your desired border radius here
       ),
-      child: const Padding(
-        padding: EdgeInsetsDirectional.only(
+      child: Padding(
+        padding: const EdgeInsetsDirectional.only(
           top: 8,
           start: 10,
           end: 10,
         ),
         child: Row(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 75,
             ),
-            Icon(
+            const Icon(
               Icons.subject_outlined,
               color: whiteColor,
               size: 30,
             ),
-            SizedBox(
+            const SizedBox(
               width: 70,
             ),
-            Text(
+            const Text(
               'Unheard Voices',
               style: TextStyle(
                 color: whiteColor,
                 fontSize: 20,
               ),
             ),
-            SizedBox(
-              width: 68,
+            const SizedBox(
+              width: 46,
             ),
-            Icon(
-              Icons.language,
-              color: whiteColor,
-              size: 26,
-            ),
+
+            PopupMenuButton(
+                padding: EdgeInsets.zero,
+                icon: const Icon(
+                  Icons.language,
+                  color: whiteColor,
+                  
+                ),
+                itemBuilder: (context) => [
+                      const PopupMenuItem(
+                        value: "en-Us",
+                        child: Text("English"),
+                      ),
+                      const PopupMenuItem(
+                        value: "ar-Eg",
+                        child: Text("Arabic"),
+                      ),
+                    ])
           ],
         ),
       ),
+    );
+  }
+}
+
+class LanguageSwitcher extends StatelessWidget {
+  const LanguageSwitcher({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<String>(
+      itemBuilder: (BuildContext context) => [
+        const PopupMenuItem<String>(
+          child: Text('English'),
+        ),
+        const PopupMenuItem<String>(
+          child: Text('Arabic'),
+        ),
+        // Add more languages as needed
+      ],
+      icon: const Icon(Icons.language),
     );
   }
 }
